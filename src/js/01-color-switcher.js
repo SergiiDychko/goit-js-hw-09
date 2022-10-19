31aeb55dae2c22f63465f7895a71ca2278437f74
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-10-19 16:10:07 +0300

    task1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startRef = document.querySelector('[data-start]');
+const stopRef = document.querySelector('[data-stop]');
+stopRef.disabled = true;
+
+function changeColor() {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = `${randomColor}`;
+}
+
+startRef.addEventListener('click', () => {
+  oneSecInterval = setInterval(changeColor, 1000);
+    startRef.disabled = true;
+    stopRef.disabled = false;
+});
+
+stopRef.addEventListener('click', () => {
+    clearInterval(oneSecInterval);
+    startRef.disabled = false;
+    stopRef.disabled = true;
+});
