bbb16613a1d1c92188fd49cb0199a6d046b7685e
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-10-20 13:17:27 +0300

    task3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+  promise
+    .then(value => Notify.success(value))
+    .catch(error => Notify.failure(error));
+}
+
+const submitBtnRef = document.querySelector('button[type="submit"]');
+
+function mySubmit(event) {
+  event.preventDefault();
+  const delayRef = Number(document.querySelector('input[name="delay"]').value);
+  const stepRef = Number(document.querySelector('input[name="step"]').value);
+  const amountRef = Number(
+    document.querySelector('input[name="amount"]').value
+  );
+  for (let i = 0; i < amountRef; i += 1) {
+    let promiseNum = i + 1;
+    let promiseDelay = delayRef + stepRef * i;
+    createPromise(promiseNum, promiseDelay);
   }
 }
+
+submitBtnRef.addEventListener('click', mySubmit);
