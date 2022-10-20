6012dfff07a6489a9a89e36832ae2edb1ebe1e2d
SergiiDychko <110810294+SergiiDychko@users.noreply.github.com>
2022-10-20 10:48:58 +0000

    Deploying to gh-pages from @ SergiiDychko/goit-js-hw-09@e0bf31ced44fc0b22da74bbef7df5c04953e7cc7 🚀

---
+++ 01-color-switcher.96fbf0d4.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");function n(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor="".concat(e)}e.addEventListener("click",(function(){oneSecInterval=setInterval(n,1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(oneSecInterval),e.disabled=!1,t.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.96fbf0d4.js.map
